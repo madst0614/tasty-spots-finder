947d48f7aff855fb3a57a60f4b585cb656bc35b9
12hyeon <hyeonjeong1212@naver.com>
2023-10-31 21:38:48 +0900

    chore: skeleton

---
+++ src/test/java/wanted/n/NApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
