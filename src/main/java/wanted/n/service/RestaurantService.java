4366b49ff327a32f17249dbf24d7da9bedb9f4ce
HyunsooZo <bzhs1992@icloud.com>
2023-11-02 01:48:56 +0900

    style: 주석 보완 및 불필요한 주석 제거

--- src/main/java/wanted/n/service/RestaurantService.java
+++ src/main/java/wanted/n/service/RestaurantService.java
@@ -7,27 +7,18 @@ import wanted.n.dto.ReviewRequestDTO;
 import wanted.n.exception.CustomException;
 import wanted.n.exception.ErrorCode;
 import wanted.n.repository.RestaurantRepository;
-import wanted.n.repository.querydsl.RestaurantQuerydslRepository;
 
 @RequiredArgsConstructor
 @Service
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
-    private final RestaurantQuerydslRepository restaurantQuerydslRepository;
-
-//    public Page<RestaurantSearchResponseDTO> searchList(RestaurantSearchRequestDTO restaurantSearchRequestDTO, Pageable pageable) {
-//        return restaurantQuerydslRepository.searchList(restaurantSearchRequestDTO, pageable);
-//    }
-//
-//    public RestaurantDetailResponseDTO getDetail(String id) {
-//        Restaurant findRestaurant = restaurantRepository.findById(id)
-//                .orElseThrow(() -> new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
-//        return RestaurantDetailResponseDTO.from(findRestaurant);
-//    }
 
     /**
      * restaurant 평점, 리뷰 수 업데이트
+     *
+     * @param reviewRequestDTO 레스토랑 ID와 새로운 평점 정보를 포함하는 데이터.
+     * @throws CustomException 지정된 레스토랑을 찾을 수 없는 경우 발생.
      */
     @Transactional
     public void updateRate(ReviewRequestDTO reviewRequestDTO){
