58dcf6ad5066829a2c9a3e4e873542b10c102adc
sh <madst0614@gmail.com>
2023-11-04 02:02:20 +0900

    style: 불필요한 어노테이션 삭제

--- src/main/java/wanted/n/domain/User.java
+++ src/main/java/wanted/n/domain/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
+import wanted.n.dto.UserSignUpRequestDTO;
 import wanted.n.enums.UserRole;
 import wanted.n.enums.UserStatus;
 
@@ -22,15 +23,13 @@ public class User extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    @NotNull
-    private String nickname;
-
     @Column(unique = true)
     private String email;
 
-    @Column
     private String password;
+
+    @NotNull
+    private String nickname;
 
     @Column(name = "user_role")
     @Enumerated(EnumType.STRING)
@@ -40,12 +39,23 @@ public class User extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
-    @Column
     private Double lat;
-
-    @Column
     private Double lon;
 
     @Column(name = "lunch_served")
     private Boolean lunchServed;
+
+    public static User from(UserSignUpRequestDTO userSignUpRequestDTO){
+        return User.builder()
+                .email(userSignUpRequestDTO.getEmail())
+                .password(userSignUpRequestDTO.getPassword())
+                .nickname(userSignUpRequestDTO.getNickname())
+                .userRole(UserRole.ROLE_USER)
+                .userStatus(UserStatus.UNVERIFIED)
+                .lat(userSignUpRequestDTO.getLat())
+                .lon(userSignUpRequestDTO.getLon())
+                .lunchServed(userSignUpRequestDTO.getLunchServed())
+                .build();
+
+    }
 }
