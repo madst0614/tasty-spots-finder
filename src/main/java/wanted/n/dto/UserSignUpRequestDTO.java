91170696f61c89d14986d6f0155a803e9ff4273b
sh <madst0614@gmail.com>
2023-11-04 02:08:38 +0900

    feat: User 관련 DTO 추가

---
+++ src/main/java/wanted/n/dto/UserSignUpRequestDTO.java
@@ -0,0 +1,38 @@
+package wanted.n.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserSignUpRequestDTO {
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @Size(min = 10, max = 20, message = "비밀번호는 10자 이상 20자 이하로 입력해 주세요.")
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력해 주세요.")
+    private String nickname;
+
+    private Double lat;
+    private Double lon;
+
+    @NotNull
+    private Boolean lunchServed;
+
+    public void setPassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+}
