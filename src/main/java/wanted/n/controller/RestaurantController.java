3dccbbd0f195cd4a353986ac2df8ca2dd39130ef
kkzz0001 <kkzz0001@gmail.com>
2023-11-03 19:19:52 +0900

    refactor: 컨트롤러 리턴값 변경

--- src/main/java/wanted/n/controller/RestaurantController.java
+++ src/main/java/wanted/n/controller/RestaurantController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +33,7 @@ public class RestaurantController {
 
     @GetMapping("/{restaurantId}")
     @ApiOperation(value = "맛집 상세정보", notes = "맛집 상세정보")
-    public RestaurantDetailResponseDTO getDetail(@PathVariable("restaurantId") String restaurantId){
-        return restaurantService.getDetail(restaurantId);
+    public ResponseEntity<RestaurantDetailResponseDTO> getDetail(@PathVariable("restaurantId") String restaurantId){
+        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.getDetail(restaurantId));
     }
 }
