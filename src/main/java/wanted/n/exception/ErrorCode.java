b68f421714457e9e5e5c9ef4c6c90a5350f756f6
sh <madst0614@gmail.com>
2023-11-04 02:05:21 +0900

    feat: Errorcode 추가

--- src/main/java/wanted/n/exception/ErrorCode.java
+++ src/main/java/wanted/n/exception/ErrorCode.java
@@ -12,7 +12,14 @@ public enum ErrorCode {
     UNDEFINED_EXCEPTION(HttpStatus.BAD_REQUEST, "알 수 없는 오류입니다."),
     POSTING_NOT_FOUND(HttpStatus.NOT_FOUND, "Posting이 존재하지 않습니다."),
     INVALID_PAGINATION_OFFSET(HttpStatus.BAD_REQUEST, "page offset에 음수가 들어갈 수 없습니다."),
-    INVALID_PAGINATION_SIZE(HttpStatus.BAD_REQUEST, "page size에 음수가 들어갈 수 없습니다.");
+    INVALID_PAGINATION_SIZE(HttpStatus.BAD_REQUEST, "page size에 음수가 들어갈 수 없습니다."),
+
+    //User Exception
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일 입니다"),
+
+    //Token Exception
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다.");
 
     private final HttpStatus status;
     private final String message;
