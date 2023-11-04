e6c0e82a9efcd2b92987c0be42f7f24c958f9331
madst0614 <81747963+madst0614@users.noreply.github.com>
2023-11-04 23:45:08 +0900

    Revert "merge: 인증 및 인가 기능 추가 (#13)"
    This reverts commit 7dbff4bf90b072f3c93e15eff0b427240cc3d089.

--- src/main/java/wanted/n/domain/User.java
+++ src/main/java/wanted/n/domain/User.java
@@ -1,11 +1,10 @@
 package wanted.n.domain;
 
-import wanted.n.dto.UserSignUpRequestDTO;
 import lombok.*;
 import wanted.n.enums.UserRole;
 import wanted.n.enums.UserStatus;
+
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -18,42 +17,33 @@ public class User extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column
+    private String account;
+
+    @Column
     private String email;
 
+    @Column
     private String password;
 
-    @NotNull
-    private String nickname;
-
-    @Column(name = "user_role")
+    @Column
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-    @Column(name = "user_status")
+    @Column
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
+    @Column
     private Double lat;
+
+    @Column
     private Double lon;
 
-    @Column(name = "lunch_served")
-    private Boolean lunchServed;
-  
+    @Column
+    private Boolean lunch_served;
+
     @OneToMany(mappedBy = "user")
     private List<Review> reviewList;
 
-    public static User from(UserSignUpRequestDTO userSignUpRequestDTO){
-        return User.builder()
-                .email(userSignUpRequestDTO.getEmail())
-                .password(userSignUpRequestDTO.getPassword())
-                .nickname(userSignUpRequestDTO.getNickname())
-                .userRole(UserRole.ROLE_USER)
-                .userStatus(UserStatus.UNVERIFIED)
-                .lat(userSignUpRequestDTO.getLat())
-                .lon(userSignUpRequestDTO.getLon())
-                .lunchServed(userSignUpRequestDTO.getLunchServed())
-                .build();
-
-    }
 }
