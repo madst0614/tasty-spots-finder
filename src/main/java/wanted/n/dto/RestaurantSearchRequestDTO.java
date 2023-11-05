3c78acc3129c0d91df5e146e64024d36e5797fe8
kkzz0001 <kkzz0001@gmail.com>
2023-11-05 23:35:11 +0900

    feat: 맛집 리스트 조회 기능 추가

---
+++ src/main/java/wanted/n/dto/RestaurantSearchRequestDTO.java
@@ -0,0 +1,25 @@
+package wanted.n.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@ApiModel(value = "restaurant request", description = "맛집 리스트 조회시 요청 정보")
+public class RestaurantSearchRequestDTO {
+
+    @NotNull(message = "lat 필드는 필수 입력 항목입니다.")
+    private Double lat;
+
+    @NotNull(message = "lat 필드는 필수 입력 항목입니다.")
+    private Double lon;
+
+    @NotNull(message = "range 필드는 필수 입력 항목입니다.")
+    private Double range;
+
+    private String orderBy;
+}
--- src/main/java/wanted/n/service/RestaurantService.java
+++ src/main/java/wanted/n/service/RestaurantService.java
@@ -17,10 +17,21 @@ public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
 
+    /**
+     *  지정된 위치에서 범위 내 맛집리스트 조회
+     *
+     *  필수 검색조건
+     *  lat : 위도
+     *  lon : 경도
+     *  range : 검색범위 (km)
+     */
     public Page<RestaurantSearchResponseDTO> searchList(RestaurantSearchRequestDTO restaurantSearchRequestDTO, Pageable pageable) {
         return restaurantRepository.findBySearch(restaurantSearchRequestDTO, pageable);
     }
 
+    /**
+     *  맛집 상세정보 조회
+     */
     public RestaurantDetailResponseDTO getDetail(String id) {
         return restaurantRepository.findByIdWithReviewList(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
